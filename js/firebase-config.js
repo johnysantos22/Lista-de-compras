231f6bf76d3abf4d233be2683e88df4a68bd9b2f
johnysantos22 <santosjohny302@gmail.com>
2026-04-20 10:10:00 -0300

    Publicar firebase-config.js e remover arquivo exemplo

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// js/firebase-config.js
+
+export const firebaseConfig = {
+    apiKey: 'AIzaSyAjlnXlBn2V0AAeZnAcQX4c8aZJsS_UIAM',
+    authDomain: 'lista-de-compras-b46d9.firebaseapp.com',
+    projectId: 'lista-de-compras-b46d9',
+    storageBucket: 'lista-de-compras-b46d9.firebasestorage.app',
+    messagingSenderId: '912317277074',
+    appId: '1:912317277074:web:302ce431308fc789ef974b',
+    measurementId: 'G-WB84H9EFE8'
+};
