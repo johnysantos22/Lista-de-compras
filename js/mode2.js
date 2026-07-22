f33af96003b64138e4ebfec91964ea37d78cfb9e
johnysantos22 <santosjohny302@gmail.com>
2026-07-22 20:50:55 -0300

    add butao de editar

--- js/mode2.js
+++ js/mode2.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js';
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-analytics.js';
 import {
-  getFirestore, collection, addDoc, getDocs, getDocsFromServer, query, where, deleteDoc, doc, serverTimestamp
+  getFirestore, collection, addDoc, getDocs, getDocsFromServer, query, where, deleteDoc, doc, serverTimestamp, updateDoc
 } from 'https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js';
 import {
   getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged
